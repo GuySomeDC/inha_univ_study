fa9ed55285a540fe98ed25301ee647bd74d5f251
GuySomeDC <guysomedc@gmail.com>
2021-12-05 13:28:46 +0900

    Add print matrix

--- matrix-chain_multiplication/README.md
+++ matrix-chain_multiplication/README.md
@@ -3,5 +3,4 @@
 n = matrix의 개수  
 p = p0, p1, p2, ...  
 ### output
-dp  
 result
--- matrix-chain_multiplication/main.py
+++ matrix-chain_multiplication/main.py
@@ -1,15 +1,26 @@
 def matrix(i, j):  # Ai부터 Aj까지 multiplication 횟수
-    if dp[i - 1][j - 1] != float('inf'):
-        return dp[i - 1][j - 1]
+    if dp[i - 1][j - 1][0] != float('inf'):
+        return dp[i - 1][j - 1][0]
     if i == j:
-        dp[i - 1][j - 1] = 0
-        return dp[i - 1][j - 1]
-    if j - i == 1:
-        dp[i - 1][j - 1] = p[i - 1] * p[i] * p[j]
-        return dp[i - 1][j - 1]
-    result = min([matrix(i, k) + matrix(k + 1, j) + p[i - 1] * p[k] * p[j] for k in range(i, j)])
-    dp[i - 1][j - 1] = result
-    return dp[i - 1][j - 1]
+        dp[i - 1][j - 1][0] = 0
+        dp[i - 1][j - 1][1] = i
+        return dp[i - 1][j - 1][0]
+    result = float('inf')
+    min_k = 0
+    for k in range(i, j):
+        r = matrix(i, k) + matrix(k + 1, j) + p[i - 1] * p[k] * p[j]
+        if r < result:
+            result = r
+            min_k = k
+    dp[i - 1][j - 1][0] = result
+    dp[i - 1][j - 1][1] = min_k
+    return dp[i - 1][j - 1][0]
+
+
+def print_optimal_parens(i, j):
+    if i == j:
+        return f'A{i}'
+    return '(' + print_optimal_parens(i, dp[i - 1][j - 1][1]) + print_optimal_parens(dp[i - 1][j - 1][1] + 1, j) + ')'
 
 
 if __name__ == '__main__':
@@ -20,8 +31,7 @@ if __name__ == '__main__':
     for _ in range(0, n):
         print(f'A{_ + 1}={p[_]}x{p[_ + 1]}')
 
-    dp = [[float('inf') for __ in range(0, n)] for _ in range(0, n)]
+    dp = [[[float('inf'), 0] for __ in range(0, n)] for _ in range(0, n)]
     matrix(1, n)
-    for d in dp:
-        print(d)
-    print(dp[0][n - 1])
+    print(dp[0][n - 1][0])
+    print(print_optimal_parens(1, n))
