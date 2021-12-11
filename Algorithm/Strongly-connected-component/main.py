2fe0d410899d8ea0ae21447d0f200034c9b03089
GuySomeDC <guysomedc@gmail.com>
2021-12-11 21:50:17 +0900

    Add strongly connected component

---
+++ Algorithm/Strongly-connected-component/main.py
@@ -0,0 +1,28 @@
+def dfs(start):
+    if visit[start]:
+        return
+    print(alphabet[start])
+    visit[start] = True
+
+    for g in graph[start]:
+        dfs(g)
+    stack.append(start)
+
+
+if __name__ == '__main__':
+    alphabet = ' ABCDEFGHIJKLMNOPQRSTUVWXTZ'
+    n = int(input('n='))  # number of vertices
+    graph = [[] for _ in range(n + 1)]
+    stack = []
+    visit = [False for _ in range(n + 1)]
+    while True:
+        edge = input('edge=')  # edge v1->v2
+        if edge == 'end':
+            break
+        v1, v2 = map(int, edge.split())  # edge v1->v2
+        graph[v1].append(v2)
+    dfs(4)
+    for i in range(1, n + 1):
+        if not visit[i]:
+            dfs(i)
+    print([alphabet[s] for s in stack])
